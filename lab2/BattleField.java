1e89bb6cd93784f86beea5c76d70dc1f68299098
losososos <112784018+losososos@users.noreply.github.com>
2022-12-06 23:18:41 +0300

    Update BattleField.java

--- lab2/BattleField.java
+++ lab2/BattleField.java
@@ -12,9 +12,9 @@ public class BattleField extends Eevee {
     public static void main(String[] args) {
         Battle b = new Battle();
         Pokemon p1 = new Hawlucha("a", 30);
-        Pokemon p2 = new Hawlucha("b", 30);
-        Pokemon p3 = new Hawlucha("c", 30);
-        Pokemon p4 = new Hawlucha("d", 30);
+        Pokemon p2 = new Lombre("b", 30);
+        Pokemon p3 = new Espeon("c", 30);
+        Pokemon p4 = new Ludicolo("d", 30);
 
         b.addAlly(p1);
         b.addAlly(p2);
